2e6ba25f74ee488dde2cb6b7de9e96931991e591
sriramsupreeth2006 <sriram.supreethd@gmail.com>
2025-10-11 10:42:00 +0530

    Experiment 5 done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-function Header() {
-  return <h1>Welcome to My React App</h1>;
-}
-function Content() {
-  return <p>This is a simple React application with multiple components.</p>;
-}
-function Footer() {
-  return <footer>©2025 React Experiment 4 - React Components</footer>;
-}
+import './App.css';
+import Navbar from './Navbar.js';
+import Home from './Home.js';
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Content />
-      <Footer />
-    </div>
-    );
+return (
+<div className="App">
+<Navbar/>
+<div className="content">Hello Everyone! Welcome to KL University</div>
+<Home />
+</div>
+);
 }
 export default App;
