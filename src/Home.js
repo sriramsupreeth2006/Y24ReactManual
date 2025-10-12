ce2a5c4fe72c62c3842858b477755b0b5b5a3c25
sriramsupreeth2006 <sriram.supreethd@gmail.com>
2025-10-12 11:12:16 +0530

    Experiment 13 done

--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,16 @@ const Home = () => {
         {title:"Mission Impossible 8", body:"Action Spy", author:"Christopher Mcquarrie", id:2},
         {title:"Jathiratnalu", body:"Comedy", author:"Anudeep KV", id:3}
     ]);
+    const handleDelete = (id) => {
+        const newBlogs=blogs.filter(blog =>
+blog.id !== id); 
+        setBlogs(newBlogs);
+}
     return (
     <div className="home">
         < BlogList blogs={blogs} title="All Blogs!" />
         < BlogList blogs={blogs.filter((blog) => blog.author ===
-'Karthik Ghattamneni')} title="Karthik's blogs" />
+'Karthik Ghattamneni')} title="Karthik's blogs" handleDelete={handleDelete} />
      </div>
         );       
 }
