9dd531d2bf5639cee0c780c0bddee41dd669c63e
sriramsupreeth2006 <sriram.supreethd@gmail.com>
2025-10-12 20:22:05 +0530

    Experiment 28 done

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,8 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
+
 const BlogDetails = () => {
   const { id } = useParams();
+  const history = useHistory();
   const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+
+  const handleClick = () => {
+    fetch('http://localhost:8000/blogs/' + blog.id, {
+      method: 'DELETE'
+    }).then(() => {
+      history.push('/')
+    })
+  }
+
   return (
     <div className="blog-details">
       {isPending && <div>Loading...</div>}
@@ -12,9 +23,11 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>delete</button>
         </article>
       )}
     </div>
   );
 }
+
 export default BlogDetails;
