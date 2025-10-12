58be2a6e27415b61d3ca74a0ea9aa02aa9842cfb
sriramsupreeth2006 <sriram.supreethd@gmail.com>
2025-10-12 15:51:31 +0530

    Experiment 18 done

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,34 @@
 import { useState, useEffect } from "react";
 import BlogList from './BlogList';
-
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-
-  useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => res.json())
-      .then(data => setBlogs(data));
-  }, []);
-
-  return (
-    <div className="home">
-      { !blogs && <div>Loading...</div> }
-      { blogs && <BlogList blogs={blogs} title="All Blogs!" /> }
-    </div>
-  );
+const [blogs,setBlogs]=useState(null);
+const [isPending, setIsPending]=useState(true);
+const [error,setError]=useState(null);
+useEffect(() => {
+setTimeout( () => {
+fetch('http://localhost:8000/blogs')
+.then(res => {
+if (!res.ok){
+throw Error("Could not fetch data from the resource");
 }
-
+return res.json();
+})
+.then(data => {
+setBlogs(data);
+setIsPending(false);
+})
+.catch(err => {
+setIsPending(false)
+setError(err.message)
+})
+}, 5000);
+},[]);
+return (
+  <div className="home">
+{ error && <div> {error} </div>}
+{ isPending && <div>Loading... </div> }
+{ blogs && <BlogList blogs={blogs} title="All Blogs!"/> }
+</div>
+);
+}
 export default Home;
