549bd916f2e6511c48229c2c26cc1ac36505184b
sriramsupreeth2006 <sriram.supreethd@gmail.com>
2025-10-12 12:37:05 +0530

    Experiment 17 done

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,21 @@
 import { useState, useEffect } from "react";
 import BlogList from './BlogList';
+
 const Home = () => {
-const [blogs,setBlogs]=useState(null);
-useEffect(() => {
-fetch('http://localhost:8000/blogs')
-.then(res => {
-return res.json();
-})
-.then(data => {
-setBlogs(data);
-})
-},[]);
-return (
-<div className="home">
-{ blogs && <BlogList blogs={blogs}
-title="All Blogs!"/> }
-</div>
-);
+  const [blogs, setBlogs] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then(res => res.json())
+      .then(data => setBlogs(data));
+  }, []);
+
+  return (
+    <div className="home">
+      { !blogs && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} title="All Blogs!" /> }
+    </div>
+  );
 }
+
 export default Home;
