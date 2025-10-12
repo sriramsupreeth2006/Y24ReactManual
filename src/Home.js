4733df7aa722903044b06a508cf6070d0a2c08fb
sriramsupreeth2006 <sriram.supreethd@gmail.com>
2025-10-12 10:32:54 +0530

    Experiment 11 done

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from './BlogList';
 const Home = () => {
     const [blogs,setBlogs]=useState([
         {title:"Mirai", body:"Action Fantasy", author:"Karthik Ghattamneni", id:1},
@@ -7,14 +8,8 @@ const Home = () => {
     ]);
     return (
     <div className="home">
-        {blogs.map((blog) => (
-            <div className="blog-preview" key={blog.id}>
-            <h2>{blog.title}</h2>
-            <p>Written by: {blog.author}</p>
-            </div>
-        ))
-        }
-        </div>
-    );
+        <BlogList blogs={blogs} title="All Blogs!" />
+     </div>
+        );       
 }
 export default Home;
