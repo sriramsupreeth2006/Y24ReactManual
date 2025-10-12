6fa95546f3ee8e4f93656ebb9207d84c950dc5a1
sriramsupreeth2006 <sriram.supreethd@gmail.com>
2025-10-12 16:48:23 +0530

    Experiment 21 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 
 import Navbar from './Navbar.js';
 import Home from './Home.js';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Create from './Create.js';
 function App() {
 return (
+<Router>
 <div className="App">
 <Navbar/>
-<div className="content">Hello Everyone! Welcome to KL University</div>
+<div className="content">Hello World</div>
+<Switch>
+<Route exact path="/">
 <Home />
+</Route>
+<Route path="/create">
+<Create />
+</Route>
+</Switch>
 </div>
-);
-}
+</Router>
+);}
 export default App;
