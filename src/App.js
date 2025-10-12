0e5152d1bdcc0daff54e67d74daa8f34eae56931
sriramsupreeth2006 <sriram.supreethd@gmail.com>
2025-10-12 20:32:12 +0530

    Experiment 29 done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
+
 import Navbar from './Navbar.js';
 import Home from './Home.js';
-import { BrowserRouter as Router, Route, Switch} from
-'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create.js';
 import BlogDetails from './BlogDetails.js';
+import Notfound from './NotFound.js';
+
 function App() {
-return (
-<Router>
-<div className="App">
-<Navbar/>
-<div className="content">Hello World</div>
-<Switch>
-<Route exact path="/">
-<Home />
-</Route>
-<Route path="/create">
-<Create />
-</Route>
-<Route path="/blogs/:id">
-<BlogDetails/>
-</Route>
-</Switch>
-</div>
-</Router>
-);
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <Notfound />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
 }
 export default App;
