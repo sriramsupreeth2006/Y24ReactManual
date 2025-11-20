b1f0acefbdf485159ad4964932383ff6e50f513c
sriramsupreeth2006 <sriram.supreethd@gmail.com>
2025-11-20 20:11:22 +0530

    Experiment 34 second version code done

--- files.js
+++ files.js
@@ -1,6 +1,9 @@
 const http = require('http');
 const server= http.createServer((req,res) => {
-console.log('request made')
+console.log(req.url,req.method)
+res.setHeader('Content-Type','text/plain');
+res.write('Hello World');
+res.end()
 })
 server.listen(3000,'localhost', () => {
 console.log('listening for requests on port 3000');
