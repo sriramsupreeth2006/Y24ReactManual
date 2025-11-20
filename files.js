e8f04df6ade6df66293c567923d0c4daabd59dea
sriramsupreeth2006 <sriram.supreethd@gmail.com>
2025-11-20 19:19:24 +0530

    Experiment 32 writing files done

--- files.js
+++ files.js
@@ -1,8 +1,4 @@
 const fs = require('fs');
-fs.readFile('./blog1.txt',(err, data) => {
-if(err){
-console.log(err)
-}
-console.log(data.toString());
-});
-console.log('last line');
+fs.writeFile('./blog1.txt','I am Gemini',() => {
+console.log('file was written')
+})
