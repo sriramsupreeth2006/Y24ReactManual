b5393db91fbc565678b8a45f97bf0c5f33cd923f
sriramsupreeth2006 <sriram.supreethd@gmail.com>
2025-11-20 18:53:50 +0530

    Experiment 31 done

--- module.js
+++ module.js
@@ -1,2 +1,2 @@
-const xyz = require('./people.js')
-console.log(xyz)
+const os = require('os');
+console.log(os.platform(), os.homedir());
--- people.js
+++ people.js
@@ -1,3 +1,4 @@
 const people = ['srikant','avinash','akhil'];
+const ages = [41, 16, 22];
 console.log(people);
-module.exports = people;
+module.exports = { people, ages };
